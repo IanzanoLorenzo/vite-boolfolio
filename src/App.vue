af6d25ff90ace53ea19fc6aa7978298df89f169d
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-08-29 17:42:50 +0200

    chiamata axios effettuata + create store

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <script>
-import ProjectsIndexVue from "./components/ProjectsIndex.vue";
+import axios from 'axios';
+import {store} from './store';
+import ProjectMain from "./components/ProjectMain.vue";
 export default{
   components:{
-    ProjectsIndexVue,
+    ProjectMain,
+  },
+  data(){
+    return {
+      axios,
+      store
+    }
+  },
+  mounted(){
+    axios.get(store.basicUrl+'/api/projects').then((risp) => {
+      store.projects = risp.data.response
+      console.log(store.projects)
+    })
   }
 };
 </script>
 
 <template>
-  <ProjectsIndexVue></ProjectsIndexVue>
+  <ProjectMain></ProjectMain>
 </template>
 
 <style lang="scss">
---
+++ src/components/ProjectMain.vue
@@ -0,0 +1,20 @@
+<script>
+import {store} from '../store'
+export default{
+  components:{
+    ProjectsIndexVue,
+  },
+  data(){
+    return {
+      store
+    }
+  }
+};
+</script>
+
+<template>
+    
+</template>
+
+<style lang="scss">
+</style>
